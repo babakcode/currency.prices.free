2382e5851802deaaaa22b105cf4536a5626e7094
Babak <babakcode@gmail.com>
2024-12-04 04:06:08 +0330

    fixed: - edit url issue

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/babakcode/currency.prices.free',
+            'https://github.com/babakcode/currency.prices.free/blob/docs/',
         },
         blog: {
           showReadingTime: true,
@@ -52,7 +52,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/babakcode/currency.prices.free',
+            'https://github.com/babakcode/currency.prices.free/blob/docs/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',

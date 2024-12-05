e382fcda5608b230e907987aab54c525f571ce61
Babak <babakcode@gmail.com>
2024-12-05 21:16:21 +0330

    changed favicon and social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -132,7 +132,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Currency BabakCode, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
